86ed11dbd72db4eb55461d208cbab400088f24f8
M. Haroon Mughal <harryjacker6@gmail.com>
2020-01-03 18:49:12 +0500

    studentbooksname

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -32,7 +32,7 @@ class Header extends Component {
                     onHideNav={()=>this.onHideNav()}
                 />
                 <Link to="/" className="logo">
-                    The Book Shelf
+                    Student Books
                 </Link>
             </header>
         );
